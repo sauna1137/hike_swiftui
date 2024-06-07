591af85c3aa23615b45134299822f58339df849b
sauna1137 <subscription.search@gmail.com>
2024-06-07 22:48:43 +0900

    Initial Commit

---
+++ hike_swiftui/hike_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hike_swiftuiApp.swift
+//  hike_swiftui
+//
+//  Created by KS on 2024/06/07.
+//
+
+import SwiftUI
+
+@main
+struct hike_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
