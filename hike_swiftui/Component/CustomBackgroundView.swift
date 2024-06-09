e076733e3c21fd33cfac5c5f02ff375f265de49d
sauna1137 <subscription.search@gmail.com>
2024-06-10 07:23:45 +0900

    additional card view

--- hike_swiftui/Component/CustomBackgroundView.swift
+++ hike_swiftui/Component/CustomBackgroundView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct CustomBackgroundView: View {
     var body: some View {
         ZStack {
+            // Depth 3
+
+            Color.customGreenDark
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 40, height: 40)))
+                .offset(y: 8)
+
+            // Light 2
+
+            Color.customGreenLight
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 40, height: 40)))
+                .offset(y: 4)
+                .opacity(0.3)
+
+            // Surface 1
             LinearGradient(
                 colors:
-                    [Color("ColorGreenLight"),
-                     Color("ColorGreenMedium")],
+                    [.customGreenLight,
+                     .customGreenMedium],
                 startPoint: .top,
                 endPoint: .bottom)
             .clipShape(RoundedRectangle(cornerSize: CGSize(width: 40, height: 40)))
