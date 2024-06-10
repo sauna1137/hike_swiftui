ef1c0d9970896215b99cb0bd755ab7a27d62ea2b
sauna1137 <subscription.search@gmail.com>
2024-06-11 06:26:38 +0900

    add customButtonView

--- hike_swiftui/Component/CardView.swift
+++ hike_swiftui/Component/CardView.swift
@@ -11,18 +11,51 @@ struct CardView: View {
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                    LinearGradient(colors: [Color("ColorIndigoMedium"),
-                                            Color("ColorSarmon")], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
+            VStack {
+                // Header
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+                            )
 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                        Spacer()
+
+                        Button {
+                            // action sheet
+                            print("tapped")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+
+                    Text("Fun and enjoynable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundStyle(.colorGrayMedium)
+                } // end header
+                .padding(.horizontal, 30)
+
+                // Main Content
+
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color("ColorIndigoMedium"),
+                                                    Color("ColorSarmon")], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                // footer
             }
-        }
+        } // card
         .frame(width: 320, height: 570)
     }
 }
